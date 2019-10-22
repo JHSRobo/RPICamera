fae0c155a5d4543351bc432a78f741fa55277372
ajgrinds <ajgrinds03@gmail.com>
2019-10-22 15:48:07 -0700

    Added post method and form values and json

--- test.py
+++ test.py
@@ -5,12 +5,12 @@
 # streams to port 80 on the raspberry pi
 
 import io
-import picamera
-import logging
 import socketserver
 from threading import Condition
 from http import server
 import json
+
+# Change these values to update them in the code
 
 try:
     with open('config.json', mode='r') as json_file:
@@ -76,28 +76,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+
         else:
             self.send_error(404)
             self.end_headers()
@@ -133,26 +112,20 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_response(301)
             self.send_header('Location', '/index.html')
             self.end_headers()
-        except
+        except Exception as e:
+            print(e)
             self.send_error(404, 'Bad request submitted.')
 
         self.end_headers()
+
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
 
-with picamera.PiCamera(resolution=data['resoloution'], framerate=data['FPS']) as camera:
-    output = StreamingOutput()
-    # Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    camera.rotation = data['rotation']
-    camera.start_recording(output, format='mjpeg')
-    try:
-        port = 80
-        address = ('', port)
-        print(f"Streaming on port {port}")
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+port = 80
+address = ('', port)
+print(f"Streaming on port {port}")
+server = StreamingServer(address, StreamingHandler)
+server.serve_forever()
