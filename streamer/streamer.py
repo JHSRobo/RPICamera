0c2d16aeba958cce32f57d5e7dea3ace9705776a
ajgrinds <ajgrinds03@gmail.com>
2019-10-24 15:02:59 -0700

    Added form restart

--- streamer/streamer.py
+++ streamer/streamer.py
@@ -12,132 +12,151 @@ from threading import Condition
 from http import server
 import json
 
+defaults = {'FPS': '60', 'rotation': '0', 'resolution': '640x480'}
 
-def main():
-    try:
-        with open('config.json', mode='r') as json_file:
-            data = json.load(json_file)
-    except FileNotFoundError:
-        with open("config.json", mode='w') as config:
-            json.dump({'FPS': '60', 'rotation': '0', 'resoloution': '640x480'}, config)
 
-    def writer(filename: str, dictionary: dict):
-        with open(filename, mode='w') as config:
-            config.truncate()
-            json.dump(dictionary, config, indent=4)
-            return config
+def writer(filename: str, dictionary: dict):
+    with open(filename, mode='w') as config:
+        config.truncate()
+        json.dump(dictionary, config, indent=4)
+        return config
 
-    PAGE = """\
-    <html>
-    <body>
-    <center><img src="stream.mjpg" width="100%" height="auto"></center>
-    <br><br>
-    <center><form method="post">
-    Rotation<br><input type="text" name="rotation">
-    <br><br>
-    FPS<br><input type="text" name="FPS"> 
-    <br><br>
-    Resolution<br><input type="text" name"resloution"> 
-    <br>
-    <input type="submit"></form>
-    </body>
-    </html>
-    """
 
-    class StreamingOutput(object):
-        def __init__(self):
-            self.frame = None
-            self.buffer = io.BytesIO()
-            self.condition = Condition()
+PAGE = """\
+<html>
+<body>
+<center><img src="stream.mjpg" width="100%" height="auto"></center>
+<br><br>
+<center><form method="post">
+Rotation<br><input type="text" name="rotation">
+<br><br>
+FPS<br><input type="text" name="FPS"> 
+<br><br>
+Resolution<br><input type="text" name="resolution"> 
+<br>
+<input type="submit"></form>
+</body>
+</html>
+"""
 
-        def write(self, buf):
-            if buf.startswith(b'\xff\xd8'):
-                # New frame, copy the existing buffer's content and notify all
-                # clients it's available
-                self.buffer.truncate()
-                with self.condition:
-                    self.frame = self.buffer.getvalue()
-                    self.condition.notify_all()
-                self.buffer.seek(0)
-            return self.buffer.write(buf)
 
-    class StreamingHandler(server.BaseHTTPRequestHandler):
-        def do_GET(self):
-            if self.path == '/':
-                self.send_response(301)
-                self.send_header('Location', '/index.html')
-                self.end_headers()
-            elif self.path == '/index.html':
-                content = PAGE.encode('utf-8')
-                self.send_response(200)
-                self.send_header('Content-Type', 'text/html')
-                self.send_header('Content-Length', len(content))
-                self.end_headers()
-                self.wfile.write(content)
-            elif self.path == '/stream.mjpg':
-                self.send_response(200)
-                self.send_header('Age', 0)
-                self.send_header('Cache-Control', 'no-cache, private')
-                self.send_header('Pragma', 'no-cache')
-                self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-                self.end_headers()
-                try:
-                    while True:
-                        with output.condition:
-                            output.condition.wait()
-                            frame = output.frame
-                        self.wfile.write(b'--FRAME\r\n')
-                        self.send_header('Content-Type', 'image/jpeg')
-                        self.send_header('Content-Length', len(frame))
-                        self.end_headers()
-                        self.wfile.write(frame)
-                        self.wfile.write(b'\r\n')
-                except Exception as e:
-                    logging.warning(
-                        'Removed streaming client %s: %s',
-                        self.client_address, str(e))
-            else:
-                self.send_error(404)
-                self.end_headers()
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
 
-        def do_POST(self):
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
             try:
-                content_length = int(self.headers['Content-Length'])
-                data_input = bytes.decode(self.rfile.read(content_length))
-                data_input = data_input.split("&")
-                for val in data_input:
-                    val = val.split('=')
-                    if val[0] == 'FPS':
-                        try:
-                            if int(val[1]) > 120 or int(val[1]) < 10:
-                                # Please use a number between 10 and 120
-                                continue
-                        except ValueError:
-                            continue
-                    elif val[0] == 'resolution':
-                        try:
-                            width, height = val[1].split("x")
-                        except ValueError:
-                            # Please use WIDTHxHEIGHT
-                            pass
-                    elif val[0] == 'rotation':
-                        try:
-                            if int(val[1]) > 360:
-                                val[1] = str(int(val[1]) % 360)
-                        except ValueError:
-                            pass
-                    data[val[0]] = val[1]
-                writer("config.json", data)
-                main()
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
             except Exception as e:
-                self.send_error(404, 'Error: {}'.format(e))
-
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
             self.end_headers()
 
-    class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-        allow_reuse_address = True
-        daemon_threads = True
+    def do_POST(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+            data_input = bytes.decode(self.rfile.read(content_length))
+            data_input = data_input.split("&")
+            change = False
+            for val in data_input:
+                val = val.split('=')
+                if val[0] == 'FPS':
+                    try:
+                        if int(val[1]) > 120 or int(val[1]) < 10:
+                            # Please use a number between 10 and 120
+                            continue
+                    except ValueError:
+                        continue
+                elif val[0] == 'resolution':
+                    try:
+                        width, height = val[1].split("x")
+                    except ValueError:
+                        # Please use WIDTHxHEIGHT
+                        pass
+                elif val[0] == 'rotation':
+                    try:
+                        if int(val[1]) > 360:
+                            val[1] = str(int(val[1]) % 360)
+                    except ValueError:
+                        pass
+                if data[val[0]] != val[1]:
+                    change = True
+                    data[val[0]] = val[1]
+            if change:
+                writer("config.json", data)
+                main()
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        except Exception as e:
+            self.send_error(404, 'Error: {}'.format(e))
 
+        self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+try:
+    with open('config.json', mode='r') as json_file:
+        data = json.load(json_file)
+except FileNotFoundError:
+    with open("config.json", mode='w') as config:
+        json.dump({'FPS': '60', 'rotation': '0', 'resolution': '640x480'}, config)
+else:
+    if len(data.keys()) != len(defaults.keys()):
+        with open("config.json", mode='w') as config:
+            json.dump(defaults, config)
+
+
+def main():
     with picamera.PiCamera(resolution=data['resolution'], framerate=data['FPS']) as camera:
         output = StreamingOutput()
         camera.rotation = data['rotation']
