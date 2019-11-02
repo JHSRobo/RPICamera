c12e27b7feae99a7e611e6254911ea4ae9e6c4e1
CallMeJai <32986920+CallMeJai@users.noreply.github.com>
2019-11-02 10:06:44 -0700

    Update startup.sh

--- streamer/startup.sh
+++ streamer/startup.sh
@@ -3,8 +3,8 @@
 # Script to be run at EVERY boot
 
 # Attempt to pull latest repo
-cd /home/pi/rpicamera/ || exit
+cd /home/camera/rpicamera/ || exit
 git pull
 
 # Start up camera streamer
-python3 /home/pi/rpicamera/streamer/streamer.py &
+python3 /home/camera/rpicamera/streamer/streamer.py &
