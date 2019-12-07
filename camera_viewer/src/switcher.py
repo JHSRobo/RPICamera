511544246345642e93fb9321f73e271bbf57759a
ajgrinds <ajgrinds03@gmail.com>
2019-12-07 10:07:06 -0800

    Restructed cameras

--- camera_viewer/src/switcher.py
+++ camera_viewer/src/switcher.py
@@ -9,14 +9,96 @@ import json
 import requests
 import signal
 import numpy as np
+import thread
 import rospy
 from std_msgs.msg import UInt8
 
-# NEED TO ADD ROS ERRORS
 # NEED TO ADD SENSOR DATA
 # NEED TO ADD CONFIGURATION
 # Overlay and timer stack
 # Task specific visuals - overlay
+
+
+class SwitchCameras:
+    def __init__(self):
+        # can't put on one line because then they reference each other
+        self.verified = {}
+        self.failed = {}
+        try:
+            with open("config.json") as config:
+                data = json.load(config)
+        except IOError:
+            rospy.logwarn("Please make config.json if you want to save settings")
+        else:
+            for index in data['ip_addresses']:
+                if verify(ip_address=data['ip_addresses'][index]):
+                    self.verified[index] = data['ip_addresses'][index]
+                else:
+                    self.failed[index] = data['ip_addresses'][index]
+            [rospy.logerr('Camera at {} failed, will try again'.format(self.failed[value])) for value in self.failed]
+
+        self.find_cameras()
+        if not self.verified:
+            rospy.logfatal("No cameras available, quitting")
+            self.wait_for_cameras()
+        self.num = list(self.verified.keys())[0]
+        self.cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(self.verified[num]))
+
+    def wait_for_cameras(self):
+        while not self.verified:
+            self.find_cameras()
+
+    def find_cameras(self):
+        """Finds any cameras on the current networks"""
+        verified_address = []
+        current_address = self.verified.values()
+        for i in range(2, 255):
+            if '192.168.1.{}'.format(i) in current_address:
+                continue
+            if verify('192.168.1.{}'.format(i)):
+                verified_address.append('192.168.1.{}'.format(i))
+
+        available = []
+        for j in range(1, 8):
+            if str(j) not in self.verified:
+                available.append(j)
+
+        if available:
+            for ip in verified_address:
+                rospy.loginfo('Camera detected at {}, added under {}'.format(ip, available[0]))
+                try:
+                    self.verified[available.pop(0)] = ip
+                except IndexError:
+                    break
+        else:
+            rospy.loginfo("Cameras detected, but all slots filled")
+
+    def change_camera(self, camera_num):
+        self.num = camera_num.data
+        self.cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(self.verified[self.num]))
+
+    def read(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            self.camera_failed()
+            return False
+        else:
+            cv2.namedWindow("Camera Feed", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("Camera Feed", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            cv2.putText(frame, str(self.num), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            return frame
+
+    def camera_failed(self):
+        rospy.logerr("Camera at {} has failed, please switch to a different camera".format(self.verified[self.num]))
+        self.failed[self.num] = self.verified[self.num]
+        self.verified.pop(self.num, None)
+        try:
+            self.num = list(self.verified)[0]
+            self.cap.release()
+            self.cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(self.verified[self.num]))
+        except IndexError:
+            rospy.logfatal("All cameras have failed")
+
 
 
 class GracefulKiller:
@@ -28,37 +110,6 @@ class GracefulKiller:
 
     def exit_gracefully(self, signum, frame):
         self.kill_now = True
-
-
-def send_msg(msg):
-    """Method to send messages. Right now just prints"""
-    rospy.loginfo(msg)
-    return True
-
-
-def scan(failed_dict, verified_dict):
-    """Scans all of the addresses in failed_dict to see if any camera came back online and adds them to verified dict"""
-    for index in failed_dict:
-        try:
-            # Check if a camera is online and add it if so
-            r = requests.get('http://{}:80/index.html'.format(failed_dict[index]), timeout=0.025)
-        except requests.Timeout or requests.ConnectionError:
-            continue
-        else:
-            if r.status_code == 200:
-                if index not in verified_dict:
-                    verified_dict[index] = failed_dict[index]
-                    send_msg('Camera at {} is online, added under {}'.format(failed_dict[index], index))
-                    failed_dict.pop(index, None)
-                else:
-                    for j in range(1, 8):
-                        if str(j) not in verified_dict:
-                            verified_dict[str(j)] = failed_dict[index]
-                            send_msg('Camera at {} is online, added under {}'.format(failed_dict[index], j))
-                            failed_dict.pop(index, None)
-                            break
-                    else:
-                        send_msg('Camera online at {} all cameras are full'.format(failed_dict[index]))
 
 
 def blank_frame(frame1):
@@ -73,10 +124,7 @@ def verify(ip_address):
     except requests.ConnectTimeout:
         return False
     else:
-        if r.status_code == 200:
-            return True
-        else:
-            return False
+        return True if r.status_code == 200 else False
 
 
 def show_all(cameras):
@@ -103,104 +151,22 @@ def show_all(cameras):
     return frame[0]
 
 
-def find_cameras(ip_addresses):
-    """Finds any cameras on the current networks"""
-    verified_address = []
-    current_address = ip_addresses.values()
-    for i in range(2, 255):
-        if '192.168.1.{}'.format(i) in current_address:
-            continue
-        try:
-            r = requests.get('http://192.168.1.{}:80/index.html'.format(i), timeout=0.05)
-        except requests.ConnectionError or requests.ReadTimeout:
-            continue
-        else:
-            if r.status_code == 200:
-                verified_address.append('192.168.1.{}'.format(i))
-
-    available = []
-    for j in range(1, 8):
-        if str(j) not in ip_addresses:
-            available.append(j)
-
-    if available:
-        for ip in verified_address:
-            send_msg('Camera detected at {}, added under {}'.format(ip, available[0]))
-            try:
-                ip_addresses[available.pop(0)] = ip
-            except IndexError:
-                break
-    else:
-        send_msg("Cameras detected, but all slots filled")
-
-
 def main():
     # ROS Setup
     rospy.init_node('pilot_page')
-
-    def change_camera(camera_num):
-        global cap, num
-        num = camera_num.data
-        cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(verified[num]))
-    rospy.Subscriber('/rov/camera_select', UInt8, change_camera)
-
-    # Camera Setup
-    try:
-        with open("config.json") as config:
-            data = json.load(config)
-    except IOError:
-        rospy.logwarn("Please make config.json if you want to save settings")
-        verified = {}
-        failed = {}
-        data = {}
-    else:
-        # can't put on one line because they reference each other
-        verified = {}
-        failed = {}
-        for index in data['ip_addresses']:
-            if verify(ip_address=data['ip_addresses'][index]):
-                verified[index] = data['ip_addresses'][index]
-            else:
-                failed[index] = data['ip_addresses'][index]
-        [rospy.logerr('WARNING: Camera at {} failed, will try again'.format(failed[value])) for value in failed]
-
-    find_cameras(verified)
-
-    if not verified:
-        rospy.logfatal("No cameras available, quitting")
-        return
-    global cap, num
-    num = list(verified.keys())[0]
-    cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(verified[num]))
+    rospy.Subscriber('/rov/camera_select', UInt8, switcher.change_camera)
         
     # Showing camera
     killer = GracefulKiller()
     while not killer.kill_now:
-        ret, frame = cap.read()
-        if not ret:
-            rospy.logerr("Camera at {} has failed, please switch to a different camera".format(verified[num]))
-            failed[num] = verified[num]
-            verified.pop(num, None)
-            try:
-                num = list(verified)[0]
-                cap.release()
-                cap = cv2.VideoCapture('http://{}:80/stream.mjpg'.format(verified[num]))
-            except IndexError:
-                rospy.logfatal("All cameras have failed")
-                return
-        else:
-            cv2.namedWindow("Camera Feed", cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty("Camera Feed", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            try:
-                cv2.putText(frame, "{}: {}".format(data['description'][num], str(num)), (20, 40),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            except KeyError:
-                cv2.putText(frame, str(num), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        frame = switcher.read()
+        if frame:
             cv2.imshow('Camera Feed', frame)
-
         cv2.waitKey(1)
-
-        
-if __name__ == '__main__':
-    main()
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    switcher = SwitchCameras()
+    thread.start_new_thread(main)
+    thread.start_new_thread(switcher.find_cameras)
