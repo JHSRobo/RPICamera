01ae409c19035a409efd5525b1cdaf69e58b287e
ajgrinds <ajgrinds03@gmail.com>
2020-01-24 14:19:32 -0800

    Attempted new stream

--- streamer/h264.sh
+++ streamer/h264.sh
@@ -10,7 +10,7 @@ do
   export "${output?}"
 done
 
-raspivid -n -ih -t 0 -rot $ROTATION -w $WIDTH -h $HEIGHT -fps $FPS -b 1000000 -o - | ncat -lkv4 $PORT
+raspivid -n -ih -t 0 -rot $ROTATION -w $WIDTH -h $HEIGHT -fps $FPS -b 1000000 -qp 10 -o tcp://0.0.0.0:5000
 # -n to not show the video on the Raspberry Pi display
 # -ih to insert H.264 headers into the stream
 # -t 0 to keep streaming forever
