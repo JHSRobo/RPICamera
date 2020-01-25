56791696b280453d894c0d94034b5ac52414b7b7
ajgrinds <ajgrinds03@gmail.com>
2020-01-25 10:27:14 -0800

    removed drc

--- streamer/h264.sh
+++ streamer/h264.sh
@@ -10,7 +10,7 @@ do
   export "${output?}"
 done
 
-raspivid -n -cd MJPEG -ex fixedfps -ifx none -drc med -b 25000000 -br 60 -t 0 -rot $ROTATION -w $WIDTH -h $HEIGHT -fps $FPS -o - | nc -lkv4 $PORT
+raspivid -n -cd MJPEG -ex fixedfps -ifx none -b 25000000 -br 60 -t 0 -rot $ROTATION -w $WIDTH -h $HEIGHT -fps $FPS -o - | nc -lkv4 $PORT
 
 # -n to not show the video on the Raspberry Pi display
 # -ih to insert H.264 headers into the stream
