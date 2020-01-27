b979a521f989812e48f11725b7de7c77dd71bd37
ajgrinds <ajgrinds03@gmail.com>
2020-01-26 17:10:15 -0800

    Added gpu memory

--- streamer/raspivid.sh
+++ streamer/raspivid.sh
@@ -3,7 +3,6 @@
 export ROTATION=0
 export WIDTH=1280
 export HEIGHT=720
-export FPS=90
 
 # load in environment variables from config.json
 for output in $(jq -r 'keys[] as $k | "\($k)=\(.[$k])"' config.json)
@@ -11,7 +10,7 @@ do
   export "${output?}"
 done
 
-raspivid -n -cd MJPEG -ex fixedfps -ifx none -b 25000000 -br 60 -t 0 -rot $ROTATION -w $WIDTH -h $HEIGHT -fps $FPS -o - | nc -lkv4 5000
+raspivid -n -cd MJPEG -ex fixedfps -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps 45 -o - | nc -lkv4 5000
 
 # -n to not show the video on the Raspberry Pi display
 # -ih to insert H.264 headers into the stream
