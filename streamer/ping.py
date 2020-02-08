7a9244f541b8b451b349f6ef2d5d4c9a0b01980e
ajgrinds <ajgrinds03@gmail.com>
2020-02-08 12:23:56 -0800

    Http

--- streamer/ping.py
+++ streamer/ping.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-r = requests.post(url="192.168.1.100:80")
+r = requests.post(url="http://192.168.1.100:80")
 while r.status_code != 200:
     time.sleep(1)
-    r = requests.post(url="192.168.1.100:80")
+    r = requests.post(url="http://192.168.1.100:80")
