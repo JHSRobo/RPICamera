8c20fe6e8fafc13422ccbb5c8ee17dc396f5fb2e
ajgrinds <ajgrinds03@gmail.com>
2020-02-08 13:52:29 -0800

    Pings forever

--- streamer/ping.py
+++ streamer/ping.py
@@ -1,13 +1,6 @@
 import requests
 import time
 
-try:
-    r = requests.get(url="http://192.168.1.100:12345").status_code
-except Exception:
-    r = 1
-while r != 200:
+while True:
     time.sleep(1)
-    try:
-        r = requests.get(url="http://192.168.1.100:12345").status_code
-    except Exception:
-        r = 1
+    requests.get(url="http://192.168.1.100:12345")
