7d4ee57098bb68b6d4fb6af49c96e7f6f31b9198
Alden Parker <ajparker1401@gmail.com>
2020-02-08 10:21:36 -0800

    Test script forn camera with ip select

--- streamer/h264-viewer.py
+++ streamer/h264-viewer.py
@@ -1,9 +1,11 @@
 import cv2
 
-cap = cv2.VideoCapture("http://192.168.1.45:5000/")
+camera_ip = input("Ending IP Number? >> ")
+
+cap = cv2.VideoCapture("http://192.168.1." + str(camera_ip) + ":5000/")
 #cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'))
-#cv2.namedWindow("Camera Feed", cv2.WND_PROP_FULLSCREEN)
-#cv2.setWindowProperty("Camera Feed", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.namedWindow("Camera Feed", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Camera Feed", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 while cap.isOpened():
     ret, frame = cap.read()
     if frame is None:
