ef777d8fd3a329c9f048650d90c7380d74720ef8
ajgrinds <ajgrinds03@gmail.com>
2020-02-08 12:48:00 -0800

    Change

--- change.sh
+++ change.sh
@@ -5,3 +5,5 @@ git reset --hard
 rm -rf ../ROVMIND/ros_workspace/src/camera_viewer
 mv camera_viewer ../ROVMIND/ros_workspace/src
 echo "Move complete"
+source ../ROVMIND/ros_workspace/devel/setup.bash
+rosrun camera_viewer switcher.py
