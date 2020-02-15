ed0c933b9f60cf0019ecdedc9142a0ba17407fca
ajgrinds <ajgrinds03@gmail.com>
2020-02-15 11:29:12 -0800

    Added rename

--- streamer/setup.sh
+++ streamer/setup.sh
@@ -25,18 +25,7 @@ python -m pip install requests
 if [[ "${USER}" == "pi" ]]
 then
   #setup for all of the camera modules
-
-  # rename the pi to camera and the last 2 digits of the mac address
-  MAC=$(cat /sys/class/net/eth0/address)
-
-  echo -e "127.0.0.1       localhost
-  ::1             localhost ip6-localhost ip6-loopback
-  ff02::1         ip6-allnodes
-  ff02::2         ip6-allrouters
-
-  127.0.1.1      camera${MAC: -2}" > /etc/hosts
-
-  echo "camera${MAC: -2}" > /etc/hostname
+  bash rename.sh
 
   # change the password of the pi
   useradd -m -d /home/camera/ -s /bin/bash -G sudo camera
