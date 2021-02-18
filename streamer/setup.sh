0845a3616d728ea660d3b835c5ffb7dc60a2bd73
ajgrinds <ajgrinds03@gmail.com>
2021-02-18 11:24:22 -0800

    Added more comments

--- streamer/setup.sh
+++ streamer/setup.sh
@@ -2,29 +2,39 @@
 
 # Script to be run ONCE at the first boot
 
+# Checks sudo perms needed for installing libraries, editing files and changing passwords
 if [[ "$(id -u)" != 0 ]]
   then echo "Please run as root"
   exit
 fi
 
+# Pings google. if google breaks, we don't have access to start a new camera, sorry. Needs the internet and
+# this was the best way I could think of. If you have a better way to for it
+# TODO
 if [[ "$(echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1)" == 0 ]]; then
     echo "Please connect to the internet to install libraries"
     exit
 fi
 
+# Makes sure the PI is up to date. This could cause things to break in the future
 apt update -y
 
+# Checks if ncat and nmap are installed and then installs them if they are not
+# Needed for the raspivid function to stream the camera
 dpkg -l | grep ncat || apt install ncat -y
 dpkg -l | grep nmap || apt install nmap -y
 
 
 # Safety checks
+# Dont wanna go through that again
+# Assumes that if the device name is pi then it must be a pi0
 if [[ -d "/home/pi" ]]
 then
-  # make sure the folder has the right file and edit rc.local
-  (echo -en "#!/bin/bash -e \nbash "; echo -n "$(find "/home/pi" -iname "rpicamera/streamer/startup.sh" | head -1)"; echo -en " &\nexit 0") > /etc/rc.local 
+  # make sure the folder has the right file and edit rc.local to start up the streamer on boot
+  (echo -en "#!/bin/bash -e \nbash "; echo -n "$(find "/home/pi" -iname "rpicamera" | head -1)"; echo -en "/streamer/startup.sh &\nexit 0") > /etc/rc.local 
 
   # change the password for the pi account
+  # cuz safety lol but plaintext
   echo -e "JHSRobo\nJHSRobo" | passwd pi
 
   # enable cameras
@@ -34,19 +44,20 @@ then
   echo -e "$(sed '/gpu_mem/d' /boot/config.txt)" > /boot/config.txt
   echo "gpu_mem=356" >> /boot/config.txt
 
-  # turn off the red light
+  # turn off the red light. if you leave it on, it reflects off the glass
   echo "disable_camera_led=1" >> /boot/config.txt
 
   # reboot
   echo "#Setup" >> /etc/rc.local
   reboot now
 else
-  # main pi setup
+  # Setup for the main pi instead of the camera pis
+  # We use the crontab instead of RC local to work better with ROS and ubuntu mate instead of raspbian
   # doesn't rename the pi and doesn't restart it which should allow it to not break the whole thing
-  # NOTE: REQUIRES THE FOLDER TO BE NAME rpicamera
+  # "Whoops lol" - Andrew
 
   # edit rc.local
-  echo -e "@reboot bash /home/jhsrobo/rpicamera/streamer/startup.sh" > /var/spool/cron/crontabs/root
+  echo -en "@reboot bash "; echo -n "$(find "/home/jhsrobo" -iname "rpicamera" | head -1)"; echo -en "/streamer/startup.sh" > /var/spool/cron/crontabs/root
 
   # reboot
   echo "Setup complete, please reboot"
