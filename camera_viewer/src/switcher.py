51c7f40b15cf7a42c3127d9ca0bd343292f4f1e1
ajgrinds <ajgrinds03@gmail.com>
2021-05-09 12:42:56 -0700

    Refactored code and added comments

--- camera_viewer/src/switcher.py
+++ camera_viewer/src/switcher.py
@@ -18,19 +18,28 @@ from sensor_msgs.msg import Image
 # Task specific visuals - overlay
 
 
-class SwitchCameras:
-    """The class for handling all the camera logic. Switches and reads the camera, adding an overlay to it"""
+class CameraSwitcher:
+    """The class for handling all the camera logic. Switches and reads the
+    camera, adding an overlay to it.
+    """
     def __init__(self):
+        # self.verified is a dictionary that has keys of camera numbers and
+        # values of ip addresses -
+        # self.verified = {
+        #  1: "192.168.1.101",
+        #  2: "192.168.1.102"
+        # }
         self.verified = {}
+        # self.num is an integer that represents the current camera number and
+        # is the key for self.verified
         self.num = 0
-        self.ip = ""
         self.change = False
         self.cap = None
 
         try:
             self.config = json.load(open("config.json"))
         except IOError:
-            rospy.logerr("Please make config.json if you want to save camera settings")
+            rospy.logwarn("camera_viewer: please make config.json if you want to save camera settings")
             self.config = {}
 
         self.camera_sub = rospy.Subscriber('/rov/camera_select', UInt8, self.change_camera_callback)
@@ -40,47 +49,60 @@ class SwitchCameras:
 
         self.camera_thread.start()
 
+    @property
+    def ip(self):
+        """Ensures that the IP of the camera is always the correct number
+        without sacraficing redability. Otherwise, returns False
+        """
+        try:
+            return self.verified[self.num]
+        except IndexError:
+            rospy.logerr("camera_viewer: passed a camera number that doesn't exist")
+            return False
+
     def read(self):
         """Reads a frame from the cv2 video capture and adds the overlay to it"""
         if self.change:
             self.cap.release()
             self.cap = cv2.VideoCapture('http://{}:5000'.format(self.ip))
             self.change = False
+
         ret, frame = self.cap.read()
         if frame is None:
+            rospy.logerr('camera_viewer: camera failed - please wait for it to refresh or switch cameras')
             self.change = True
             return False
-        if ret is None:
+        elif ret is None:
+            rospy.logwarn('camera_viewer: ret is None, can\'t display new frame')
             return False
-
-        cv2.putText(frame, str(self.num), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        return frame
+        else:
+            cv2.putText(frame, str(self.num), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            return frame
 
     def wait(self):
         """Waits for a camera IP to be put into verified"""
+        rospy.loginfo('camera_viewer: waiting for cameras - None connected')
         while not self.verified:
-            rospy.logwarn('Waiting for cameras - None connected')
             if rospy.is_shutdown():
                 return
+            rospy.logdebug('camera_viewer: still no cameras connected')
             time.sleep(1)
 
-        self.ip = self.verified[1]
         self.num = 1
-        rospy.loginfo("Loading capture from camera {}".format(self.num))
-        self.cap = cv2.VideoCapture('http://{}:5000'.format(self.ip))
+        rospy.loginfo("camera_viwer: loading capture from camera {}".format(self.num))
+        if self.ip:
+            self.cap = cv2.VideoCapture('http://{}:5000'.format(self.ip))
+        else:
+            # lol something is really wrong
+            rospy.logerr("camera_viwer: there is no camera at spot 1 after waiting.")
 
     def change_camera_callback(self, camera_num):
         """ROSPY subscriber to change cameras"""
         if self.num != camera_num.data:
-            try:
-                num = [x for x in self.verified if self.verified[x] == camera_num.data][0]
-            except IndexError:
-                pass
-            else:
+            if self.ip:
                 self.change = True
-                self.ip = self.verified[num]
                 self.num = num
-            rospy.logerr("Changing to camera {}".format(self.num))
+                rospy.loginfo("camera_viewer: changing to camera {}".format(self.num))
 
     def find_cameras(self):
         """Creates a web server on port 12345 and waits until it gets pinged"""
@@ -88,16 +110,16 @@ class SwitchCameras:
 
         @app.route('/', methods=["POST"])
         def page():
-            if flask.request.remote_addr not in self.verified:
+            if flask.request.remote_addr not in self.verified.values():
                 try:
                     self.verified[self.give_num(flask.request.remove_addr)] = flask.request.remote_addr
                 except IndexError:
-                    rospy.logerr('Camera detected, but there\'s no number to assign it to')
+                    rospy.logerr('camera_viewer: camera detected, but there\'s no number to assign it to')
                 else:
-                    rospy.logwarn('Camera at {}, added under {}'.format(flask.request.remote_addr,self.verified[flask.request.remote_addr]))
+                    rospy.loginfo('camera_viewer: camera at {}, added under {}'.format(flask.request.remote_addr,self.verified[flask.request.remote_addr]))
             return ""
 
-        rospy.logwarn('Camera web server online')
+        rospy.loginfo('camera_viewer: camera web server online')
         app.run(host='0.0.0.0', port=12345)
 
     def give_num(self, ip):
@@ -105,8 +127,10 @@ class SwitchCameras:
         if ip in self.config:
             return self.config[ip]
         else:
-            taken = [self.verified[x] for x in self.verified]
-            available = [x for x in range(1, 8) if x not in taken][0]
+            try:
+                available = [num for num in range(1, 8) if num not in self.verified][0]
+            except IndexError:
+                rospy.logerr('camera_viwer: camera detected, but there are no available numbers')
             return available
 
     def cleaup(self):
@@ -118,7 +142,7 @@ class SwitchCameras:
 
 def main():
     rospy.init_node('pilot_page')
-    switcher = SwitchCameras()
+    switcher = CameraSwitcher()
     switcher.wait()
 
     cv2.namedWindow("Camera Feed", cv2.WND_PROP_FULLSCREEN)
