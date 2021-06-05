57405d4f411f88888ba4e1d8873bd93e272e09c6
ajgrinds <ajgrinds03@gmail.com>
2021-06-05 14:12:07 -0700

    logging

--- camera_viewer/src/switcher.py
+++ camera_viewer/src/switcher.py
@@ -122,6 +122,7 @@ class CameraSwitcher:
 
         @app.route('/', methods=["POST", "GET"])
         def page():
+            rospy.logerr(flask.request.data)
             rospy.logdebug('camera_viewer: ping from {}'.format(flask.request.remote_addr))
             if flask.request.remote_addr not in self.verified.values():
                 self.verified[self.give_num(flask.request.remote_addr)] = flask.request.remote_addr
