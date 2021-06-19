affc03abf1c992dd660eff3ffb58e6a68e0717e3
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-06-19 11:17:59 -0700

    Update raspivid.sh

--- streamer/raspivid.sh
+++ streamer/raspivid.sh
@@ -5,7 +5,7 @@ export WIDTH=1280
 export HEIGHT=720
 export FPS=45
 
-raspivid -n -cd MJPEG -ex fixedfps -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps ${FPS} -o - | ncat -lkv4 5000
+raspivid -n -cd MJPEG -awb cloud -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps ${FPS} -o - | ncat -lkv4 5000
 
 # -n to not show the video on the Raspberry Pi display
 # -ih to insert H.264 headers into the stream
