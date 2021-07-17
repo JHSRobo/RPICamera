4e557716267ab796063995f22e867dc736aeb084
ajgrinds <ajgrinds03@gmail.com>
2021-07-17 12:42:59 -0700

    oops

--- camera_viewer/src/switcher.py
+++ camera_viewer/src/switcher.py
@@ -93,8 +93,8 @@ class CameraSwitcher:
             cv2.putText(frame, "{:.2f}".format(self.depth), (1260 - textSize[0], 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
             height = 40 + textSize[1]
             for x in self.electromags.values():
-                textSize = 1260 - cv2.getTextSize("{}: {}".format(x[1], "On" if x[0] == 1 else "Off"), cv2.FONT_HERSHEY_COMPLEX, 1, 2)[0]
-                cv2.putText(frame, "{}: {}".format(x[1], "On" if x[0] == 1 else "Off"), (textSize[0], height), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                textSize = cv2.getTextSize("{}: {}".format(x[1], "On" if x[0] == 1 else "Off"), cv2.FONT_HERSHEY_COMPLEX, 1, 2)[0]
+                cv2.putText(frame, "{}: {}".format(x[1], "On" if x[0] == 1 else "Off"), (1260 - textSize[0], height), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
                 height += textSize[1] + 20
             return frame
 
