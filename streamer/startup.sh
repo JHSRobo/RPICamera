55ccf9633b96c5f2c7faf27640eda390b41c396a
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-02-12 10:44:36 -0800

    Added version.sh to the startup

--- streamer/startup.sh
+++ streamer/startup.sh
@@ -12,3 +12,4 @@ grep -q "Setup" /etc/rc.local || ( echo "Please run setup!" && exit )
 # Start up camera streamer
 bash `find /home/pi -iname raspivid.sh || find /home/jhsrobo -iname raspivid.sh | head -1` &
 bash `find /home/pi -iname ping.sh || find /home/jhsrobo -iname ping.sh | head -1` &
+bash `find /home/pi -iname version.sh || find /home/jhsrobo -iname version.sh | head -1` &
