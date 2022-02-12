9cd2a53a114a8cd8765b07f50fe78a7e7f0033aa
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-02-12 11:12:41 -0800

    Update startup.sh
    Stopped using the 'find' method to run the shell scripts within startup

--- streamer/startup.sh
+++ streamer/startup.sh
@@ -10,6 +10,10 @@ fi
 grep -q "Setup" /etc/rc.local || ( echo "Please run setup!" && exit )
 
 # Start up camera streamer
-bash `find /home/pi -iname raspivid.sh || find /home/jhsrobo -iname raspivid.sh | head -1` &
-bash `find /home/pi -iname ping.sh || find /home/jhsrobo -iname ping.sh | head -1` &
-bash `find /home/pi -iname version.sh || find /home/jhsrobo -iname version.sh | head -1` &
+cd /home/pi/rpicamera/streamer
+bash raspivid.sh &
+bash ping.sh &
+bash version.sh &
+#bash `find /home/pi -iname raspivid.sh || find /home/jhsrobo -iname raspivid.sh | head -1` &
+#bash `find /home/pi -iname ping.sh || find /home/jhsrobo -iname ping.sh | head -1` &
+#bash `find /home/pi -iname version.sh || find /home/jhsrobo -iname version.sh | head -1` &
