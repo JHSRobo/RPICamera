c3172a6d9853f0bc3eba8a517022b4e8a09be713
James Randall <jammerand14@gmail.com>
2023-01-09 00:05:45 -0800

    Restructured repo

--- change.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull
-git reset --hard
-rm -rf ../ROVMIND/ros_workspace/src/camera_viewer
-mv camera_viewer ../ROVMIND/ros_workspace/src
-echo "Move complete"
-source ../ROVMIND/ros_workspace/devel/setup.bash
-rosrun camera_viewer switcher.py
